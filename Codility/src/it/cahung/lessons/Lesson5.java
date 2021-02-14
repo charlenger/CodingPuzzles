004e71ac057775b0523c267ade994b59e3d85096
Carlo Alberto Hung <carlo.ah@gmail.com>
2021-02-14 09:10:26 +0100

    Lesson5 GenomicRange 100%

--- Codility/src/it/cahung/lessons/Lesson5.java
+++ Codility/src/it/cahung/lessons/Lesson5.java
@@ -1,5 +1,7 @@
 package it.cahung.lessons;
 
+import java.util.Arrays;
+
 public class Lesson5 {
 
 	private final int MAX = 2000000000;
@@ -11,4 +13,64 @@ public class Lesson5 {
 		return (B / K - A / K) + (A % K == 0 ? 1 : 0);
 	}
 
+	public int[] solutionGenomicRangeQuery(String S, int[] P, int[] Q) {
+		int N = S != null ? S.length() : 0;
+		int M = P.length;
+		if (M != Q.length || M > 50000 || N > 100000 || N < 1) {
+			throw new IllegalArgumentException("P and Q must have the same length");
+		}
+		char[] genesChars = S.toCharArray();
+		int[] counterOfA = new int[N + 1];
+		int[] counterOfC = new int[N + 1];
+		int[] counterOfG = new int[N + 1];
+		int[] counterOfT = new int[N + 1];
+		int prevA = 0;
+		int prevC = 0;
+		int prevG = 0;
+		int prevT = 0;
+		for (int i = 1; i <= N; ++i) {
+			char currentChar = genesChars[i - 1];
+			counterOfA[i] = currentChar == 'A' ? ++prevA : prevA;
+			counterOfC[i] = currentChar == 'C' ? ++prevC : prevC;
+			counterOfG[i] = currentChar == 'G' ? ++prevG : prevG;
+			counterOfT[i] = currentChar == 'T' ? ++prevT : prevT;
+		}
+		int[] solution = new int[M];
+		for (int i = 0; i < M; ++i) {
+			char currentChar = 'Z';
+			int pIdx = P[i];
+			int qIdx = Q[i];
+			if (pIdx > qIdx) {
+				throw new IllegalArgumentException("Query range is not valid");
+			} else if (pIdx == qIdx) {
+				currentChar = genesChars[pIdx];
+			} else {
+				qIdx++;
+				if (counterOfA[qIdx] != counterOfA[pIdx]) {
+					currentChar = 'A';
+				} else if (counterOfC[qIdx] != counterOfC[pIdx]) {
+					currentChar = 'C';
+				} else if (counterOfG[qIdx] != counterOfG[pIdx]) {
+					currentChar = 'G';
+				} else if (counterOfT[qIdx] != counterOfT[pIdx]) {
+					currentChar = 'T';
+				}
+			}
+			switch (currentChar) {
+			case 'A':
+				solution[i] = 1;
+				break;
+			case 'C':
+				solution[i] = 2;
+				break;
+			case 'G':
+				solution[i] = 3;
+				break;
+			default:
+				solution[i] = 4;
+			}
+
+		}
+		return solution;
+	}
 }
--- Codility/src/it/cahung/lessons/Lesson5Test.java
+++ Codility/src/it/cahung/lessons/Lesson5Test.java
@@ -1,5 +1,6 @@
 package it.cahung.lessons;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
@@ -14,37 +15,37 @@ public class Lesson5Test {
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void lesson5WrongInputA() {
+	public void lesson5CountDivWrongInputA() {
 		lesson5.solutionCountDiv(-1, 1, 1);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void lesson5WrongInputAtooMuch() {
+	public void lesson5CountDivWrongInputAtooMuch() {
 		lesson5.solutionCountDiv(2000000001, 1, 1);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void lesson5WrongInputB() {
+	public void lesson5CountDivWrongInputB() {
 		lesson5.solutionCountDiv(1, -1, 1);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void lesson5WrongInputBtooMuch() {
+	public void lesson5CountDivWrongInputBtooMuch() {
 		lesson5.solutionCountDiv(1, 2000000001, 1);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void lesson5WrongInputK() {
+	public void lesson5CountDivWrongInputK() {
 		lesson5.solutionCountDiv(1, 1, 0);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void lesson5WrongInputKtooMuch() {
+	public void lesson5CountDivWrongInputKtooMuch() {
 		lesson5.solutionCountDiv(1, 1, 2000000001);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void lesson5WronInputAGreaterThanB() {
+	public void lesson5CountDivWronInputAGreaterThanB() {
 		lesson5.solutionCountDiv(2, 1, 0);
 	}
 
@@ -55,5 +56,42 @@ public class Lesson5Test {
 		assertEquals(Integer.valueOf(1000000000), Integer.valueOf(lesson5.solutionCountDiv(1, 2000000000, 2)));
 		assertEquals(Integer.valueOf(0), Integer.valueOf(lesson5.solutionCountDiv(2, 2, 5)));
 		assertEquals(Integer.valueOf(1), Integer.valueOf(lesson5.solutionCountDiv(0, 0, 2)));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void lesson5GenomicRangeQueryWrongInputDifferentPQSizes() {
+		lesson5.solutionGenomicRangeQuery("ACGT", new int[] { 1, 2, 3 }, new int[] { 2, 3 });
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void lesson5GenomicRangeQueryWrongInputPQSizesTooLong() {
+		lesson5.solutionGenomicRangeQuery("ACGT", new int[50001], new int[50001]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void lesson5GenomicRangeQueryWrongInputSTooLong() {
+		String S = new String(new char[100001]);
+		lesson5.solutionGenomicRangeQuery(S, new int[1], new int[1]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void lesson5GenomicRangeQueryWrongQuery() {
+		String S = new String("ACGTACGTACGT");
+		lesson5.solutionGenomicRangeQuery(S, new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 });
+	}
+
+	@Test
+	public void lesson5GenomicRangeQuery() {
+		assertArrayEquals(new int[] { 2, 4, 1 },
+				lesson5.solutionGenomicRangeQuery("CAGCCTA", new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 }));
+		assertArrayEquals(new int[] { 4, 2, 2 },
+				lesson5.solutionGenomicRangeQuery("TC", new int[] { 0, 0, 1 }, new int[] { 0, 1, 1 }));
+		assertArrayEquals(new int[] { 2, 2, 2 },
+				lesson5.solutionGenomicRangeQuery("CCCC", new int[] { 0, 1, 2 }, new int[] { 1, 2, 3 }));
+		assertArrayEquals(new int[] { 1, 1, 1, 2 },
+				lesson5.solutionGenomicRangeQuery("ACC", new int[] { 0, 0, 0, 1 }, new int[] { 0, 1, 2, 1 }));
+		assertArrayEquals(new int[] { 1, 1, 2 },
+				lesson5.solutionGenomicRangeQuery("AC", new int[] { 0, 0, 1 }, new int[] { 0, 1, 1 }));
+
 	}
 }
